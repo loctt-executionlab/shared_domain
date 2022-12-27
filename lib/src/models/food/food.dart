bb29775b067a68717128564286ea58927ee8824e
Loc Tr <loc.tran@executionlab.asia>
2022-12-27 11:04:49 +0700

    update food model

--- lib/src/models/food/food.dart
+++ lib/src/models/food/food.dart
@@ -3,6 +3,7 @@ import 'package:shared/src/models/food_addon/food_addon.dart';
 import 'package:shared/src/models/rating/rating.dart';
 // most likely a bug as package in imported
 // ignore: depend_on_referenced_packages
+import 'package:db/db.dart' as db;
 
 part 'food.freezed.dart';
 part 'food.g.dart';
@@ -21,4 +22,29 @@ class Food with _$Food {
   }) = _Food;
 
   factory Food.fromJson(Map<String, dynamic> json) => _$FoodFromJson(json);
+
+  factory Food.fromDB(db.Food food) => Food(
+        id: food.id.toString(),
+        name: food.name,
+        imageUrl: food.imageUrl,
+        descriptionShort: food.descriptionShort,
+        descriptionExtended: food.descriptionExtended,
+        price: food.price,
+        addons: food.addons
+            .map((e) => FoodAddon(
+                  id: e.id.toString(),
+                  name: e.name,
+                  price: e.price,
+                  description: e.description,
+                ))
+            .toList(),
+        ratings: food.ratings
+            .map((e) => Rating(
+                  userId: e.userId.toString(),
+                  userName: e.userName,
+                  content: e.content,
+                  rating: e.rating,
+                ))
+            .toList(),
+      );
 }
